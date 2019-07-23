cee0708b0c11d066a56a6ef1d108cd4b429be47d
angel-popov <angelpopov@abv.bg>
2019-07-23 10:07:02 +0300

    Added check for nexmo

--- lambda.js
+++ lambda.js
@@ -1,37 +1,43 @@
 'use strict';
 var http = require('https'); 
 
-function cr(method, path,body) {
+function cr(method, path,body,host) {
+    if (host==undefined){
+        host = 'https://nexmo-omni-channel.geeks-lab.net/cs'
+    }
     return new Promise((resolve,reject) => {
         var options = {
-            headers:{'authorization':'Bearer MDczMjdhMmU6MzNmM2IyOWM2YmZiOThlZQ=='},
+            headers:{'authorization':'Bearer MDczMjdhMmU6MzNmM2IyOWM2YmZiOThlZQ==',
+                     'content-type':'application/json'
+            },
             method: method,
         };
 
-        var req = http.request('https://nexmo-omni-channel.geeks-lab.net/cs'+path, options, res => {
-        res.setEncoding('utf8');
-        var responseString = "";
-        res.on('error', err => {
-            reject(err);
-        });
+        var req = http.request(host+path, options, res => {
+            res.setEncoding('utf8');
+            var responseString = "";
+            res.on('error', err => {
+                reject(err);
+            });
         
-        //accept incoming data asynchronously
-        res.on('data', chunk => {
-            responseString = responseString + chunk;
-        });
+            //accept incoming data asynchronously
+            res.on('data', chunk => {
+                responseString = responseString + chunk;
+            });
         
-        //return the data when streaming is complete
-        res.on('end', () => {
-            //console.log(responseString);
-            resolve( responseString);
-        });
+            //return the data when streaming is complete
+            res.on('end', () => {
+                //console.log(responseString);
+                resolve( responseString);
+            });
 
     });
-    if(body!=''){
-        console.log("sending body "+body);
-        req.write(body)
-    }
-    req.end();});
+ 
+    //console.log("sending body "+body); 
+    req.write(body);
+    req.end();
+        
+    });
 }
 
 function errorDetected(err){
@@ -41,8 +47,8 @@ function errorDetected(err){
 
 function failOnFailedValidation(res){
     if (res.filter(contains("Failed validation:")).length > 0){
-        throw new Error(res)
-    } else return res
+        throw new Error(res);
+    } else return res;
 }
 
 function negate (predicateFunc) {
@@ -59,27 +65,30 @@ function verify(description,validate){
         });
 }
 
-function shouldContain(text){ return verify("there should be text '"+text+"'",contains (text))}
-function shouldNotContain(text){ return verify('there should not be text "'+ text +'"', negate(contains (text)))}
+function shouldContain(descr,text){ return verify(descr+": there should be text '"+text+"'",contains (text))}
+function shouldNotContain(descr,text){ return verify(descr+': there should not be text "'+ text +'"', negate(contains (text)))}
 
 module.exports.handler =  async (event, context, callback) => {
     return Promise.all([
+        cr('POST','/ni/advanced/json',JSON.stringify({"api_key":"07327a2e","api_secret":"33f3b29c6bfb98ee","number":"359899041699"}),"https://api.nexmo.com")
+            .then(shouldContain ("Connect to nexmo","does not have sufficient credit"))
+            .then(shouldNotContain("Connect to nexmo","error")),
         cr('GET','/api/application','')
-            .then(shouldContain ("idd"))
-            .then(shouldNotContain("error")),
-        /* cr('POST','/api/application',
+            .then(shouldContain ("Get applications request","id"))
+            .then(shouldNotContain("Get applications request","error")),
+        cr('POST','/api/application',
                 JSON.stringify({"defaultPassword":"defaultPass","name":"test",
                 "capabilities":{"voice":{"webhooks":{"eventUrl":{"httpMethod":"POST","address":"http://eventurl"},
                 "answerUrl":{"httpMethod":"POST","address":"http://answerurl"}}}}}))
-            .then(shouldContain ("id"))
-            .then(shouldNotContain("error")), */
+            .then(shouldContain ("Create application","id"))
+            .then(shouldNotContain("Create application","error")),
             
         cr('GET','/app/index.html','')
-            .then(shouldContain("href=/cs/app/js/app"))
-            .then(shouldNotContain("error")),
+            .then(shouldContain("Get web app","href=/cs/app/js/app"))
+            .then(shouldNotContain("Get web app","error")),
         cr('GET','/ump/index.html','')
-            .then(shouldContain("href=/cs/ump/js/app"))
-            .then(shouldNotContain("error"))
+            .then(shouldContain("Get web ump","href=/cs/ump/js/app"))
+            .then(shouldNotContain("Get web ump", "error"))
         ].map (p => p.catch(errorDetected))// without this, it will stop on first error
                         ).then(failOnFailedValidation)
         
